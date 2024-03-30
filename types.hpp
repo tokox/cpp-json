43f4065d3f7b65d52c8f6a7570856c37cd1f8aaa
tokox <toko123456789@yahoo.com>
2024-03-30 23:07:05 +0100

    Fix last case of value_type in conversion to object_type

--- types.hpp
+++ types.hpp
@@ -30,7 +30,7 @@ concept object_type = std::is_same_v<T, std::nullptr_t> || std::is_same_v<T, boo
 
 
 template<value_type V>
-using object_type_from_value_type = std::conditional_t<V == Null, std::nullptr_t, std::conditional_t<V == Bool, bool, std::conditional_t<V == Int, long long int, std::conditional_t<V == Float, long double, std::conditional_t<V == String, std::string, std::conditional_t<V == Vector, std::vector<object>, std::map<std::string, object>>>>>>>;
+using object_type_from_value_type = std::conditional_t<V == Null, std::nullptr_t, std::conditional_t<V == Bool, bool, std::conditional_t<V == Int, long long int, std::conditional_t<V == Float, long double, std::conditional_t<V == String, std::string, std::conditional_t<V == Vector, std::vector<object>, std::conditional_t<V == Map, std::map<std::string, object>, std::nullptr_t>>>>>>>;
 
 template<object_type T>
 value_type value_type_from_object_type();
